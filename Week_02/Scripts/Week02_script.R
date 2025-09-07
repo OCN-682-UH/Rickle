6f8f5ec872eb3a84fda7c6dff752bd5a015a1546
szrickle <szrickle@hawaii.edu)>
2025-09-06 18:54:34 -1000

    practice push

---
+++ Week_02/Scripts/Week02_script.R
@@ -0,0 +1,17 @@
+### This is my first script. I am learning how to import data
+### Created by Samantha Rickle
+### Created on 2025-09-05
+
+### Libraries
+library(tidyverse)
+library(here)
+
+### Read in my data
+weightdata<-read_csv(here("Week_02","Data","weightdata.csv"))
+
+### Data Analysis
+head(weightdata) # looks at top 6 lines of data
+tail(weightdata) # looks at the bottom 6 lines
+view(weightdata) # shows entire data set
+
+
