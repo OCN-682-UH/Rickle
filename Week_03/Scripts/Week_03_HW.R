57112fcba1c7fcfd0c5655627cf31da5bd2ab730
szrickle <szrickle@hawaii.edu)>
2025-09-12 13:46:06 -1000

    uploading week 3 changes

--- Week_03/Scripts/Week_03_HW.R
+++ Week_03/Scripts/Week_03_HW.R
@@ -15,22 +15,22 @@ library("RColorBrewer")
 glimpse(penguins) 
 
 ### Data analysis
-ggplot(data=penguins,
-       mapping = aes(x = species,
-                     y = body_mass_g,
-                     color = species)) +
-  geom_boxplot(alpha = 0.75) +
-  geom_jitter() +
+ggplot(data=penguins, ## plotting using penguins data
+       mapping = aes(x = species, 
+                     y = body_mass_g, ## setting the x and y variables
+                     color = species)) + ## setting the color by species
+  geom_boxplot(alpha = 0.75) + ## displaying via boxplot
+  geom_jitter() + ## overlaying jitter points
   labs(title = "Body Mass According to Species",
        x = "Species",
-       y = "Body Mass (g)") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  theme(legend.position = "none",
+       y = "Body Mass (g)") + ## setting title and axis labels
+  scale_color_brewer(palette = "Dark2") + ## choosing a color palette
+  theme_bw() + ## setting background theme to black and white
+  theme(legend.position = "none", ## removing legenc bc the bars are already labeled
         title = element_text(size = 20,
-                             color = "dark blue"),
-        axis.title = element_text(size = 15,
-                                  color = "dark blue")) 
-ggsave(here("Week_03", "Output", "penguin_hw.png"))
+                             color = "dark blue"), ## changing the title text size and color
+        axis.title = element_text(size = 15, 
+                                  color = "dark blue")) ## changing the label text size and color
+ggsave(here("Week_03", "Output", "penguin_hw.png")) ## saving the plot :-)
 
        
